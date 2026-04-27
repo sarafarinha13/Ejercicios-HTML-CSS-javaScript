4819d78981db4f49f2a8a7ffa6ac1d1a61dbe7f9
Sara <150589811+sarafarinha13@users.noreply.github.com>
2026-04-28 00:15:34 +0200

    Update script.js

---
+++ slideshowGraphicDesign/script.js
@@ -0,0 +1,4 @@
+const slider = document.querySelector('.slider');
+const slide = document.querySelectorAll('.slide');
+const dotsContainer = document.querySelector('dots-container');
+let currentIndex = 0;
