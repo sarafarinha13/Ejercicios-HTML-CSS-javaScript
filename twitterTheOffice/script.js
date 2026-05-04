873bc626e70dfda386451d886175e730090079c2
Sara <150589811+sarafarinha13@users.noreply.github.com>
2026-05-04 13:29:22 +0200

    Update script.js

--- twitterTheOffice/script.js
+++ twitterTheOffice/script.js
@@ -7,19 +7,20 @@ let publicarTuit = document.getElementById("buttonTuit");
 /* Una vez llamados el boton y el tuit hacemos que cada vez que le demos a publicar se cree un post */
 publicarTuit.addEventListener("click", function(){
     let textoUsuario = inputTuit.value; /* value permite obtener o cambiar el texto que ha escrito el usuario dentro de un input o textarea */
-    createPost(textoUsuario);
+    createPost(textoUsuario, "@prisionMike", "img/1 (7).jpg");
     inputTuit.value = ""; /* se resetea y vuelve a visualizarse lo que pusimos en el placeholder */
 });    
+
 
 
 /* creamos usuarios y mensajes predeterminados */
 let usuarios = [
     {nombre: "@prankOfficeCeo", foto: "img/jim.jpg"},
     {nombre: "@receptionQueen", foto:"img/pam.jpg"},
-    {nombre: "@farmerDwight", foto:"img/dwight"},
-    {nombre: "@rylly", foto"img/ryanKelly"},
-    {nombre: "@idontcareStanley", foto"img/stanley"},
-    {nombre: "theBESTboss", foto"img/michael"}
+    {nombre: "@farmerDwight", foto:"img/dwight.jpg"},
+    {nombre: "@rylly", foto: "img/rylly.png"},
+    {nombre: "@idontcareStanley", foto: "img/stanley.jpg"},
+    {nombre: "theBESTboss", foto: "img/michael.jpg"}
 ];
 
 let mensajes = [
@@ -30,9 +31,14 @@ let mensajes = [
     "Jim stop it..."
 ];
 
-/*
-Creamos una función llamada createPost Esta función servirá para generar un nuevo post en la página */
-function createPost(textoUsuario){
+/* CREAMOS FUNCION RANDOM  */
+
+function getRandom(lista){
+    return lista[Math.floor(Math.random() * lista.length)];
+}
+
+/*Creamos una función llamada createPost Esta función servirá para generar un nuevo post en la página */
+function createPost(textoUsuario, nombre, foto){
 
     /* POST */
     // Creamos un nuevo <div> en JavaScript (aún no está en el HTML)
@@ -64,11 +70,11 @@ function createPost(textoUsuario){
     /* CONTENIDO POST_USER */
     let avatar = document.createElement("img");
         avatar.classList.add("post_avatar");
-        avatar.src= "img/1 (7).jpg";
+        avatar.src= foto; /* aqui lo igualamos a las fotos del ARRAY de USUARIOS */
 
     let username = document.createElement("p");
         username.classList.add("post_username");
-        username.innerText = "@prisionMike";
+        username.innerText = nombre;  /* aqui lo igualamos a los nombres del ARRAY de USUARIOS */
 
     post_user.appendChild(avatar);
     post_user.appendChild(username);
@@ -76,7 +82,7 @@ function createPost(textoUsuario){
     /* CONTENIDO POST_TUIT */
     let texto = document.createElement("p");
         texto.classList.add("post_texto");
-        texto.innerText = textoUsuario;
+        texto.innerText = textoUsuario; /* aqui lo igualamos al texto que entra por teclado en el buttonTuit publicar del HTML */
 
     post_tuit.appendChild(texto);
 
@@ -119,5 +125,29 @@ function createPost(textoUsuario){
         }
     });
 
+    /* CREAMOS UNA FUNCION QUE GENERE POST CON NOMBRES/ FOTOS Y MENSAJES ALEATORIOS */
+
+    function postRandom(){
+
+        let mensajesRandom = getRandom(mensajes);
+        let usuariosRandom = getRandom(usuarios);
+
+        createPost(mensajesRandom, usuariosRandom.nombre, usuariosRandom.fotoY )
+
+        setTimeout(postRandom, )
+
+       
+
+
+    }
+    setTimeout(function(){
+        let mensajesRandom = getRandom(mensajes);
+        let usuariosRandom = getRandom(usuarios);
+
+        createPost(
+            mensajesRandom, usuariosRandom.nombre, usuariosRandom.fotoY 
+        );
+
+    }, 20000);
 
 }
